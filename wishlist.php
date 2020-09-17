d42633db6f10aa04ab4dba8ee8df721ff469fcd7
Samair <samairzia1995@gmail.com>
2020-09-17 18:39:59 +0500

    wishlist

--- wishlist.php
+++ wishlist.php
@@ -71,6 +71,436 @@
               <p><a href="#!" class="theme-color">Add to Cart</a></p>
             </td>
           </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-1.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">ETUDE HOUSE – Soonjung 2x Barrier Intensive Cream 60ml</a>
+            </td>
+            <td>
+              <p>etude_123</p>
+            </td>
+            <td class="text-right">
+              <p>0.56</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
+
+          <tr>
+            <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+            <td class="text-center">
+              <a href="#!" class="cart-img"><img src="images/product-img-2.png" alt=""></a>
+            </td>
+            <td>
+              <a href="#!" class="cart-pname">THE FACE SHOP - Styling Eye Palette No.2 Fatal Burgundy</a>
+            </td>
+            <td>
+              <p>faceshop_987</p>
+            </td>
+            <td class="text-right">
+              <p>0.78</p>
+            </td>
+            <td class="text-right">
+              <p><b>￦4,550</b></p>
+            </td>
+            <td class="text-right">
+              <p><a href="#!" class="theme-color">Add to Cart</a></p>
+            </td>
+          </tr>
 
         </tbody>
       </table>
